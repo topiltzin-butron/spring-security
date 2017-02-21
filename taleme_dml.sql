d6c76d41ce5add107d665311241ee3dcbd30769d
topiltzin-butron <topiltzin-butron@users.noreply.github.com>
2017-02-21 13:38:14 +0700

    Create taleme_dml.sql

---
+++ taleme_dml.sql
@@ -0,0 +1,8 @@
+insert into greetings (user, id, creation_date, greet) values ('topi', now(), '1366354711797', 'Hola tacos')
+insert into greetings (user, id, creation_date, greet) values ('admin', now(), '1366354711797', 'Admin, do you like tacos?')
+insert into greetings (user, id, creation_date, greet) values ('dba', now(), '1366354711797', 'DBAs always want tacos')
+
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_ADMIN', 'topi', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'topi@cafeconleche.xyz', 34);
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_DBA', 'topi', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'topi@cafeconleche.xyz', 34);
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_DBA', 'alex', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'alex@cafeconleche.xyz', 27);
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_ADMIN', 'hugo', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'hugo@ehecatl.com', 35);
